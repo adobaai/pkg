3876bd422b16d8d983c3db04ab3904af3f911c0a
ionling <soarvision@outlook.com>
2025-07-17 09:00:16 +0800

    ✨ (httpz) add `JSON2` function

--- netz/httpz/httpz.go
+++ netz/httpz/httpz.go
@@ -19,6 +19,16 @@ func JSON[R any](ctx context.Context, c *http.Client, method, url string, data a
 	return DoJSON[R](c, hreq)
 }
 
+// JSON2 likes the [JSON], but can specify the error response type.
+func JSON2[R, E any](ctx context.Context, c *http.Client, method, url string, data any,
+) (res *R, er ErrorResp[E], err error) {
+	hreq, err := NewJSONRequest(ctx, method, url, data)
+	if err != nil {
+		return
+	}
+	return DoJSON2[R, E](c, hreq)
+}
+
 // RawJSON performs the request with the data marshaled to JSON format
 // and returns the raw response.
 func RawJSON(ctx context.Context, c *http.Client, method, url string, data any,
@@ -59,14 +69,35 @@ func DoJSON[R any](c *http.Client, req *http.Request) (res *R, err error) {
 	return RespJSON[R](hres)
 }
 
-// RespJSON unmarshals the response body into a new R and closes the body afterwards.
-func RespJSON[R any](r *http.Response) (res *R, err error) {
-	defer r.Body.Close()
-	bs, err := io.ReadAll(r.Body)
+// ErrorResp is the error response.
+type ErrorResp[T any] struct {
+	*http.Response
+	T *T
+}
+
+// IsZero reports whether the ErrorResp is zero.
+func (er ErrorResp[T]) IsZero() bool {
+	return er.Response == nil && er.T == nil
+}
+
+// DoJSON2 likes the [DoJSON], but can specify the error response type.
+func DoJSON2[R, E any](c *http.Client, req *http.Request) (res *R, er ErrorResp[E], err error) {
+	hres, err := c.Do(req)
 	if err != nil {
 		return
 	}
+	if hres.StatusCode >= http.StatusBadRequest {
+		e, err := RespJSON[E](hres)
+		return nil, ErrorResp[E]{hres, e}, err
+	}
+	res, err = RespJSON[R](hres)
+	return
+}
+
+// RespJSON unmarshals the response body into a new R and closes the body afterwards.
+func RespJSON[R any](r *http.Response) (res *R, err error) {
+	defer r.Body.Close()
 	res = new(R)
-	err = json.Unmarshal(bs, res)
+	err = json.NewDecoder(r.Body).Decode(res)
 	return
 }
--- netz/httpz/httpz_test.go
+++ netz/httpz/httpz_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/adobaai/pkg/testingz"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type Hello struct {
@@ -21,16 +22,38 @@ func TestJSON(t *testing.T) {
 	c := &http.Client{}
 	ctx := context.Background()
 	name := "Фёдор Миха́йлович Достое́вский"
-	testingz.R(JSON[HelloResp](ctx, c, http.MethodPost,
-		"https://httpbin.org/anything", Hello{Name: name})).
-		NoError(t).
-		Do(func(t *testing.T, it *HelloResp) {
-			assert.Equal(t, name, it.JSON.Name)
-		})
 
-	testingz.R(RawJSON(ctx, c, http.MethodGet, "https://httpbin.org/status/404", nil)).
-		NoError(t).
-		Do(func(t *testing.T, it *http.Response) {
-			assert.Equal(t, http.StatusNotFound, it.StatusCode)
-		})
+	t.Run("JSON", func(t *testing.T) {
+		testingz.R(JSON[HelloResp](ctx, c, http.MethodPost,
+			"https://httpbin.org/anything", Hello{Name: name})).
+			NoError(t).
+			Do(func(t *testing.T, it *HelloResp) {
+				assert.Equal(t, name, it.JSON.Name)
+			})
+	})
+
+	t.Run("RawJSON", func(t *testing.T) {
+		res, err := RawJSON(ctx, c, http.MethodGet, "https://httpbin.org/status/404", nil)
+		require.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, res.StatusCode)
+	})
+
+	t.Run("JSON2", func(t *testing.T) {
+		type ErrorResp struct {
+			Error struct {
+				Code    int    `json:"code"`
+				Message string `json:"message"`
+			} `json:"error"`
+		}
+
+		url := "https://api.weatherapi.com/v1/current.json"
+		res, er, err := JSON2[HelloResp, ErrorResp](ctx, c, http.MethodGet, url,
+			Hello{Name: name})
+		require.NoError(t, err)
+		assert.Nil(t, res)
+		assert.Equal(t, 401, er.StatusCode)
+		assert.Equal(t, 1002, er.T.Error.Code)
+		assert.Equal(t, "API key is invalid or not provided.", er.T.Error.Message)
+
+	})
 }
